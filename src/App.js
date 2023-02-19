a1ed90b00908fb11d94d1b684880bebc0401056c
Denny <rileydv@gmail.com>
2023-02-19 12:55:36 -0500

    Deployed website

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 
-import {
-  BrowserRouter,
-  Routes,
-  Route
-} from "react-router-dom";
+// import {
+//   BrowserRouter,
+//   Routes,
+//   Route
+// } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom"
 import Home from "./pages/home/Home";
 import Listing from "./pages/listing/Listing";
 import List from "./pages/list/List";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/listings" element={<List/>}/>
         <Route path="/listings/:id" element={<Listing/>}/>
-      </Routes>
-    </BrowserRouter>
+        </Routes>
+    </HashRouter>
+
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path="/" element={<Home/>}/>
+    //     <Route path="/listings" element={<List/>}/>
+    //     <Route path="/listings/:id" element={<Listing/>}/>
+    //   </Routes>
+    // </BrowserRouter>
   );
 }
 
